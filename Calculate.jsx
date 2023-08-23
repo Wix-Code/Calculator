7d96476bb25b3a9594e3e26118318d30b8e4f9fc
Wix-Code <140562388+Wix-Code@users.noreply.github.com>
2023-08-23 09:52:36 +0100

    Add files via upload

---
+++ Calculate.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import './calculate.css'
+
+const Calculate = () => {
+
+  const [value, setValue] = useState ('');
+
+  return (
+    <div className='calculate'>
+      <div className="calculate1">
+        <div className="cal">
+          <input className='put' type="text" value={value}/>
+        </div>
+        <div className="count">
+          <input className='color' type="button" value='AC' onClick={e => setValue('')}/>
+          <input className='color' type="button" value='DE' onClick={e => setValue(value.slice(0, -1))}/>
+          <input className='color' type="button" value='.' onClick={e => setValue(value + e.target.value)}/>
+          <input className='color' type="button" value='/' onClick={e => setValue(value + e.target.value)}/>
+        </div>
+        <div className="count">
+          <input type="button" value='7' onClick={e => setValue(value + e.target.value)}/>
+          <input type="button" value='8' onClick={e => setValue(value + e.target.value)}/>
+          <input type="button" value='9' onClick={e => setValue(value + e.target.value)}/>
+          <input className='color' type="button" value='*' onClick={e => setValue(value + e.target.value)}/>
+        </div>
+        <div className="count">
+          <input type="button" value='4' onClick={e => setValue(value + e.target.value)}/>
+          <input type="button" value='5' onClick={e => setValue(value + e.target.value)}/>
+          <input type="button" value='6' onClick={e => setValue(value + e.target.value)}/>
+          <input className='color' type="button" value='+' onClick={e => setValue(value + e.target.value)}/>
+        </div>
+        <div className="count">
+          <input type="button" value='1' onClick={e => setValue(value + e.target.value)}/>
+          <input type="button" value='2' onClick={e => setValue(value + e.target.value)}/>
+          <input type="button" value='3' onClick={e => setValue(value + e.target.value)}/>
+          <input className='color' type="button" value='-' onClick={e => setValue(value + e.target.value)}/>
+        </div>
+        <div className="count">
+          <input type="button" value='00' onClick={e => setValue(value + e.target.value)}/>
+          <input type="button" value='0' onClick={e => setValue(value + e.target.value)}/>
+          <input type="button" value='=' className='equal' onClick={e => setValue(eval(value))}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Calculate
