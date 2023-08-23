d800b042e09254eba967de721a491d295691f5a4
Wix-Code <140562388+Wix-Code@users.noreply.github.com>
2023-08-23 09:28:29 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Calculate from './calculate/Calculate';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculate/>
+    </div>
+  );
+}
+
+export default App;
